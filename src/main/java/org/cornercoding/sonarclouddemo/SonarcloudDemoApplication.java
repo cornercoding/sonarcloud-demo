05e6c263edcaa76e314b3126d8af98848d77a472
Dhrubajyoti Bhattacharjee <dhrubajyotibhattacharjee@Dhrubajyotis-MacBook-Air.local>
2018-01-07 15:10:11 +0530

    Initial boilerplate setup

---
+++ src/main/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplication.java
@@ -0,0 +1,12 @@
+package org.cornercoding.sonarclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cornercoding.sonarclouddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonarcloudDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
